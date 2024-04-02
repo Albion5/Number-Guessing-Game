d17c7ca71c27d488ff2ada0e4b4a3f9a8a72c461
Alina Safronova <amifinova@gmail.com>
2024-04-02 17:09:47 +0000

    feat: add db script

---
+++ create_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY, name VARCHAR(22) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS games (
+    game_id SERIAL PRIMARY KEY, user_id INT NOT NULL, guesses INT NOT NULL, FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
