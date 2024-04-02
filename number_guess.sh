fed401aeb35f947294f9239bd905bd6eaa2816c8
Alina Safronova <amifinova@gmail.com>
2024-04-02 18:06:25 +0000

    feat: add save game result function

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=game -t --no-align -c"
+
 MAIN_LOOP() {
   if [[ -n "$1" ]]
   then
@@ -12,15 +13,18 @@ MAIN_LOOP() {
   then
     if [[ $GUESS -eq $NUMBER ]]
     then
-      echo you won!
+      echo -e "\nYou guessed it in $GUESSES tries. The secret number was $NUMBER. Nice job!"
     else
       MAIN_LOOP "you didn't guess"
     fi
   else
-    MAIN_LOOP "try again"
+    MAIN_LOOP "That is not an integer, guess again:"
   fi
 }
 
+SAVE_GAME_RESULT() {
+  local INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES ($1, $2)")
+}
 
 GET_USER_GAME_HISTORY() {
   echo "$($PSQL "SELECT game_id FROM games WHERE user_id = $1")"
@@ -42,7 +46,7 @@ GAME() {
     if [[ -z $USER_ID ]]
     then
       USER_ID=$(REGISTER "$NAME")
-      echo "Welcome, $NAME! It looks like this is your first time here."
+      echo "Welcome, $USER_ID! It looks like this is your first time here."
     else
       GAME_HISTORY=$(GET_USER_GAME_HISTORY $USER_ID)
       echo Welcome back, $NAME! $GAME_HISTORY
@@ -50,12 +54,12 @@ GAME() {
     GUESSES=0
     NUMBER=5
     MAIN_LOOP "Guess the secret number between 1 and 1000:"
-    echo $GUESSES
+    SAVE_GAME_RESULT $USER_ID $GUESSES
   fi
 }
 
 REGISTER() {
-  $PSQL "INSERT INTO users(name) VALUES('$1')"
+  local INSERT_USER_RESULT=$($PSQL "INSERT INTO users(name) VALUES('$1')")
   echo $(LOGIN "$1")
   
 }
